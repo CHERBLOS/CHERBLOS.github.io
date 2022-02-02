5ba287a3203d7e5a16c505c2ec127d32ace5e4a4
CHERBLOS <vlad.xxxx4@gmail.com>
2022-02-02 19:55:15 +0300

    update

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import "../scss/style.scss";
-import "../js/brands.js";
-import "../js/swiper.js";
-import "../js/types.js";
-import "../js/sidebar.js";
-import "../js/modal-call.js";
-import "../js/modal-chat.js";
-
-console.log("Works!");
